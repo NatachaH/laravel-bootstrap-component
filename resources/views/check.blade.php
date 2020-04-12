f5c9c0c903ba8221d7e5b83820ee2efb3acd3a6a
Natacha Herth <info@natachaherth.ch>
2020-04-12 16:33:15 +0200

    id and for with value

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -5,12 +5,12 @@
     name="{{ $name }}"
     value="{{ $value }}"
     class="form-check-input @error($name) is-invalid @enderror"
-    id="{{ $name.'Field' }}"
+    id="{{ $name.'Field'.$value }}"
     {{ $isDisabled ? 'disabled' : ''}}
     {{ $isChecked ? 'checked' : ''}}
   />
 
-  <label class="form-check-label" for="{{ $name.'Field' }}">
+  <label class="form-check-label" for="{{ $name.'Field'.$value }}">
     {{ $label }}
   </label>
 
