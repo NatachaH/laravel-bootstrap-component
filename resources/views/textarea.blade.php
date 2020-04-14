39de45a8a1eb69c3fa8119b73066219ba2c3111d
Natacha Herth <info@natachaherth.ch>
2020-04-14 15:36:45 +0200

    Textarea

---
+++ resources/views/textarea.blade.php
@@ -0,0 +1,29 @@
+<div {{ $attributes }}>
+
+  @if($label)
+    <label for="{{ $name.'Field' }}" class="form-label">{{ $label }} @if($isRequired) <i class="text-muted">*</i> @endif</label>
+  @endif
+
+  <textarea
+    name="{{ $name }}"
+    class="form-control {{ $size }} @error($name) is-invalid @enderror"
+    id="{{ $name.'Field' }}"
+    @if($placeholder) placeholder="{{ $placeholder }}" @endif
+    @if($help) aria-describedby="{{ $name.'FieldHelp' }}" @endif
+    {{ $isReadonly ? 'readonly' : '' }}
+    {{ $isDisabled ? 'disabled' : ''}}
+    {{ $isRequired ? 'required' : ''}}
+  >
+    {{ old($name,$value) }}
+  </textarea>
+
+  @if($help)
+    <small id="{{ $name.'FieldHelp' }}" class="form-text text-muted">{{ $help }}</small>
+  @endif
+
+  @error($name)
+      <span class="invalid-feedback" role="alert">
+          {{ $message }}
+      </span>
+  @enderror
+</div>
---
+++ src/View/Components/Textarea.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Nh\BsComponent\View\Components;
+
+use Illuminate\View\Component;
+
+class Textarea extends Component
+{
+    /**
+     * The label of the textarea.
+     *
+     * @var string
+     */
+    public $label;
+
+    /**
+     * The name of the textarea.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The default value of the textarea.
+     *
+     * @var string
+     */
+    public $value;
+
+    /**
+     * The placeholder of the textarea.
+     *
+     * @var string
+     */
+    public $placeholder;
+
+    /**
+     * The help message of the textarea.
+     *
+     * @var string
+     */
+    public $help;
+
+    /**
+     * Is the textarea readonly.
+     *
+     * @var boolean
+     */
+    public $isReadonly;
+
+    /**
+     * Is the textarea disabled.
+     *
+     * @var boolean
+     */
+    public $isDisabled;
+
+    /**
+     * Is the textarea required.
+     *
+     * @var boolean
+     */
+    public $isRequired;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($label = '', $name, $value = '', $placeholder = '', $help  = '', $readonly = false, $disabled = false, $required = false )
+    {
+        $this->label        = $label;
+        $this->name         = $name;
+        $this->value        = $value;
+        $this->placeholder  = $placeholder;
+        $this->help         = $help;
+        $this->isReadonly   = $readonly;
+        $this->isDisabled   = $disabled;
+        $this->isRequired   = $required;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('bs-component::textarea');
+    }
+}
