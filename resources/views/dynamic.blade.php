894d59b0a0341d93373c82ddecd9ad06d9579b5c
Natacha Herth <info@natachaherth.ch>
2020-04-17 18:52:30 +0200

    dynamic slot

--- resources/views/dynamic.blade.php
+++ resources/views/dynamic.blade.php
@@ -2,12 +2,14 @@
 
     <legend>{{ $legend }}</legend>
 
+    {!! $slot !!}
+
     @if($currentItems)
       <div class="list-group">
         @foreach ($currentItems as $key => $current)
           <div class="list-group-item d-flex align-items-center dynamic-current">
 
-            @includeIf($folder.'.dynamic.current')
+            {!! $current !!}
 
             <div class="btn-group-toggle ml-auto" data-toggle="buttons">
               <label class="btn btn-sm btn-danger active">
@@ -30,7 +32,7 @@
 
       <div class="d-flex align-items-end dynamic-item">
 
-        @includeIf($folder.'.dynamic.form')
+         {!! $form !!}
 
         @if($isDynamic)
           <div class="flex-shrink-1 mb-1">
