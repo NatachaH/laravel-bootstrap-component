4092878ec0760d46c6f3639d0378cac93c162661
Natacha Herth <info@natachaherth.ch>
2020-04-23 18:33:39 +0200

    remove btn-sm

--- resources/views/dynamic.blade.php
+++ resources/views/dynamic.blade.php
@@ -14,7 +14,7 @@
         @endif
 
         @if($isActive)
-          <button type="button" class="ml-auto btn btn-sm dynamic-add {{ $btnAdd['class'] ?? 'btn-primary' }}" aria-label="{{ $btnAdd['label'] ?? 'Add' }}">{!! $btnAdd['value'] ?? 'Add' !!}</button>
+          <button type="button" class="ml-auto btn dynamic-add {{ $btnAdd['class'] ?? 'btn-primary' }}" aria-label="{{ $btnAdd['label'] ?? 'Add' }}">{!! $btnAdd['value'] ?? 'Add' !!}</button>
         @endif
     </div>
 
@@ -26,7 +26,7 @@
 
         @if($isActive)
           <div class="dynamic-item-btn">
-            <button type="button" class="btn btn-sm dynamic-remove {{ $btnRemove['class'] ?? 'btn-danger' }}" aria-label="{{ $btnRemove['label'] ?? 'Remove' }}">{!! $btnRemove['value'] ?? 'Remove' !!}</button>
+            <button type="button" class="btn dynamic-remove {{ $btnRemove['class'] ?? 'btn-danger' }}" aria-label="{{ $btnRemove['label'] ?? 'Remove' }}">{!! $btnRemove['value'] ?? 'Remove' !!}</button>
           </div>
         @endif
 
