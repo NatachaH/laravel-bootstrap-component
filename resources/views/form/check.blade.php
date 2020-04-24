336323d7a67eeb5fd711885807bcd46a9decaa34
Natacha Herth <info@natachaherth.ch>
2020-04-24 11:55:25 +0200

    Bootstrap v4

--- resources/views/form/check.blade.php
+++ resources/views/form/check.blade.php
@@ -1,3 +1,4 @@
+{{-- Bootstrap 5
 <div {{ $attributes->merge(['class' => 'form-check']) }}>
 
   <input
@@ -16,6 +17,40 @@
   @endif
 
   <label class="form-check-label" for="{{ $id.'Field' }}">
+    {{ $label }} @if($isRequired) <i class="text-muted">*</i> @endif
+  </label>
+
+  @if($help)
+    <small id="{{ $id.'FieldHelp' }}" class="form-text text-muted">{{ $help }}</small>
+  @endif
+
+  @error($name)
+    <span class="invalid-feedback" role="alert">
+        {{ $message }}
+    </span>
+  @enderror
+
+</div>
+--}}
+
+<div {{ $attributes->merge(['class' => 'custom-control custom-'.$type]) }}>
+
+  <input
+    type="{{ $type }}"
+    name="{{ $name }}"
+    value="{{ $value }}"
+    class="custom-control-input @error($name) is-invalid @enderror"
+    id="{{ $id.'Field' }}"
+    {{ $isDisabled ? 'disabled' : ''}}
+    {{ $isRequired ? 'required' : ''}}
+    {{ $isChecked ? 'checked' : ''}}
+  />
+
+  @if($isDisabled && $isChecked)
+    <input type="hidden" name="disabled_{{ $name }}" value="{{ $value }}"/>
+  @endif
+
+  <label class="custom-control-label" for="{{ $id.'Field' }}">
     {{ $label }} @if($isRequired) <i class="text-muted">*</i> @endif
   </label>
 
