b8664f124cb3b447d8ee071f0f2857c483045505
Natacha Herth <info@natachaherth.ch>
2020-06-17 15:44:30 +0200

    Select V5

--- resources/views/form/field/select.blade.php
+++ resources/views/form/field/select.blade.php
@@ -1,25 +1,5 @@
-{{-- Bootstrap 5
 <select
   class="form-select {{ !empty($size) ? 'form-select-'.$size : '' }} @error($cleanName) is-invalid @enderror"
-  name="{{ $name }}"
-  {{ $isMultiple ? 'multiple' : ''}}
-  {{ $isDisabled ? 'disabled' : ''}}
-  {{ $isRequired ? 'required' : ''}}
->
-
-  @foreach ($options as $key => $value)
-    <option value="{{ $key }}" {{ $isOptionSelected($key) ? 'selected' : '' }} {{ $isOptionDisabled($key) ? 'disabled' : '' }}>{{ $value }}</option>
-  @endforeach
-
-</select>
-
-@if($isDisabled && !empty($values))
-  <input type="hidden" name="disabled_{{ $name }}" value="{{ $values }}"/>
-@endif
---}}
-
-<select
-  class="custom-select {{ !empty($size) ? 'custom-select-'.$size : '' }} @error($cleanName) is-invalid @enderror"
   name="{{ $name }}"
   {{ $isMultiple ? 'multiple' : ''}}
   {{ $isDisabled ? 'disabled' : ''}}
