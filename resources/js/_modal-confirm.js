0cff08afd3992e7474a159289aa3c88bfd0309a9
Natacha Herth <info@natachaherth.ch>
2020-08-24 13:07:38 +0200

    Corr modal confirm with jquery

--- resources/js/_modal-confirm.js
+++ resources/js/_modal-confirm.js
@@ -5,17 +5,38 @@
 */
 
 // Open Confirm Modal
-var confirmModals = document.getElementsByClassName('modal-confirm');
+var confirmModals = document.querySelectorAll('.modal-confirm');
+
 confirmModals.forEach((modal, i) => {
+
+  $(modal).on('show.bs.modal', function (event) {
+    var button = event.relatedTarget
+    var action = button.getAttribute('data-action');
+    var form = modal.querySelector('form');
+
+    console.log(action);
+
+    form.action = action;
+    form.onsubmit = function(e) {
+      var loading = modal.querySelector('.spinner-border');
+      loading.classList.remove('d-none');
+    }
+  });
+
+  /*
   modal.addEventListener('show.bs.modal', function(event) {
     var button = event.relatedTarget
     var action = button.getAttribute('data-action');
     var form = modal.querySelector('form');
+
+    console.log(action);
+
     form.action = action;
     form.onsubmit = function(e) {
       var loading = modal.querySelector('.spinner-border');
       loading.classList.remove('d-none');
     }
   })
+  */
 
 });
