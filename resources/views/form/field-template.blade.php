43d579ee1054e336d782b589f1d73b28973e2158
Natacha Herth <info@natachaherth.ch>
2020-11-12 18:42:14 +0100

    Corr for input-group datepicker

--- resources/views/form/field-template.blade.php
+++ resources/views/form/field-template.blade.php
@@ -4,7 +4,7 @@
     <label for="{{ $cleanName.'Field' }}" class="form-label">{{ $label }} @if($isRequired) <i class="text-muted">*</i> @endif</label>
   @endif
 
-  @if($before or $after or $isInputGroup)
+  @if($before or $after or isset($isInputGroup))
     <div class="input-group">
         @isset($before)
           <span class="input-group-text">{!! $before !!}</span>
