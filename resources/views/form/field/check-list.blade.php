0266e14a664e4308f90d74fdfc48659f86432737
Natacha Herth <info@natachaherth.ch>
2021-03-18 11:33:02 +0100

    check-list array multiple

--- resources/views/form/field/check-list.blade.php
+++ resources/views/form/field/check-list.blade.php
@@ -2,27 +2,63 @@
 
   @foreach ($options as $key => $value)
 
-    <div {{ $attributes->merge(['class' => 'form-check '.($isInline ? 'form-check-inline' : '')]) }}>
+    @if(is_array($value))
 
-      <input
-        type="{{ $type }}"
-        name="{{ $name }}"
-        value="{{ $key }}"
-        class="form-check-input @error($cleanName,$errorBag) is-invalid @enderror @if($errorRelated) @error($errorRelated,$errorBag) is-invalid @enderror @endif"
-        id="{{ $idOption($key).'Field' }}"
-        {{ $isDisabled || $isOptionDisabled($key) ? 'disabled' : '' }}
-        {{ $isOptionChecked($key) ? 'checked' : '' }}
-      />
+      <div {{ $attributes->merge(['class' => 'form-check-group '.($isInline ? 'form-check-group-inline' : '')]) }}>
 
-      @if(($isDisabled || $isOptionDisabled($key)) && $isOptionChecked($key))
-        <input type="hidden" name="disabled_{{ $name }}" value="{{ $key }}"/>
-      @endif
+        <label>{{ $key }}</label>
+        @foreach ($value as $k => $val)
 
-      <label class="form-check-label" for="{{ $idOption($key).'Field' }}">
-        {{ $value }}
-      </label>
+          <div class="form-check {{ $isInline ? 'form-check-inline' : '' }}">
 
-    </div>
+            <input
+              type="{{ $type }}"
+              name="{{ $name }}"
+              value="{{ $k }}"
+              class="form-check-input @error($cleanName,$errorBag) is-invalid @enderror @if($errorRelated) @error($errorRelated,$errorBag) is-invalid @enderror @endif"
+              id="{{ $idOption($k).'Field' }}"
+              {{ $isDisabled || $isOptionDisabled($k) ? 'disabled' : '' }}
+              {{ $isOptionChecked($k) ? 'checked' : '' }}
+            />
+
+            @if(($isDisabled || $isOptionDisabled($k)) && $isOptionChecked($k))
+              <input type="hidden" name="disabled_{{ $name }}" value="{{ $k }}"/>
+            @endif
+
+            <label class="form-check-label" for="{{ $idOption($k).'Field' }}">
+              {{ $val }}
+            </label>
+
+          </div>
+        @endforeach
+
+      </div>
+
+    @else
+      <div {{ $attributes->merge(['class' => 'form-check '.($isInline ? 'form-check-inline' : '')]) }}>
+
+        <input
+          type="{{ $type }}"
+          name="{{ $name }}"
+          value="{{ $key }}"
+          class="form-check-input @error($cleanName,$errorBag) is-invalid @enderror @if($errorRelated) @error($errorRelated,$errorBag) is-invalid @enderror @endif"
+          id="{{ $idOption($key).'Field' }}"
+          {{ $isDisabled || $isOptionDisabled($key) ? 'disabled' : '' }}
+          {{ $isOptionChecked($key) ? 'checked' : '' }}
+        />
+
+        @if(($isDisabled || $isOptionDisabled($key)) && $isOptionChecked($key))
+          <input type="hidden" name="disabled_{{ $name }}" value="{{ $key }}"/>
+        @endif
+
+        <label class="form-check-label" for="{{ $idOption($key).'Field' }}">
+          {{ $value }}
+        </label>
+
+      </div>
+    @endif
+
+
 
   @endforeach
 
