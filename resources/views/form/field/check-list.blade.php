64536a1fb51bfa2fc612b9681f0fd2d90d681c9e
Natacha Herth <info@natachaherth.ch>
2021-03-18 11:37:08 +0100

    corr checklist

--- resources/views/form/field/check-list.blade.php
+++ resources/views/form/field/check-list.blade.php
@@ -7,31 +7,32 @@
       <div {{ $attributes->merge(['class' => 'form-check-group '.($isInline ? 'form-check-group-inline' : '')]) }}>
 
         <label>{{ $key }}</label>
-        @foreach ($value as $k => $val)
+        <div>
+          @foreach ($value as $k => $val)
 
-          <div class="form-check {{ $isInline ? 'form-check-inline' : '' }}">
+            <div class="form-check {{ $isInline ? 'form-check-inline' : '' }}">
 
-            <input
-              type="{{ $type }}"
-              name="{{ $name }}"
-              value="{{ $k }}"
-              class="form-check-input @error($cleanName,$errorBag) is-invalid @enderror @if($errorRelated) @error($errorRelated,$errorBag) is-invalid @enderror @endif"
-              id="{{ $idOption($k).'Field' }}"
-              {{ $isDisabled || $isOptionDisabled($k) ? 'disabled' : '' }}
-              {{ $isOptionChecked($k) ? 'checked' : '' }}
-            />
+              <input
+                type="{{ $type }}"
+                name="{{ $name }}"
+                value="{{ $k }}"
+                class="form-check-input @error($cleanName,$errorBag) is-invalid @enderror @if($errorRelated) @error($errorRelated,$errorBag) is-invalid @enderror @endif"
+                id="{{ $idOption($k).'Field' }}"
+                {{ $isDisabled || $isOptionDisabled($k) ? 'disabled' : '' }}
+                {{ $isOptionChecked($k) ? 'checked' : '' }}
+              />
 
-            @if(($isDisabled || $isOptionDisabled($k)) && $isOptionChecked($k))
-              <input type="hidden" name="disabled_{{ $name }}" value="{{ $k }}"/>
-            @endif
+              @if(($isDisabled || $isOptionDisabled($k)) && $isOptionChecked($k))
+                <input type="hidden" name="disabled_{{ $name }}" value="{{ $k }}"/>
+              @endif
 
-            <label class="form-check-label" for="{{ $idOption($k).'Field' }}">
-              {{ $val }}
-            </label>
+              <label class="form-check-label" for="{{ $idOption($k).'Field' }}">
+                {{ $val }}
+              </label>
 
-          </div>
-        @endforeach
-
+            </div>
+          @endforeach
+        </div>
       </div>
 
     @else
