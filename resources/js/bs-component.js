60671dc13ea164541cd8a5bbab94affaf6041837
Natacha Herth <info@natachaherth.ch>
2023-01-18 20:28:03 +0100

    Clean

--- resources/js/bs-component.js
+++ resources/js/bs-component.js
@@ -4,59 +4,30 @@
 |--------------------------------------------------------------------------
 */
 
-//import './_calendar';
-import CheckboxAll from './_checkbox-all';
 import Autocomplete from './_autocomplete';
-
-// import './_datepicker';
+import Calendar from './_calendar';
+import CheckboxAll from './_checkbox-all';
+import Datepicker from './_datepicker';
 import Dynamic from './_dynamic';
-// import './_editor';
+import Editor from './_editor';
 import ModalConfirm from './_modal-confirm';
 import ModalLoad from './_modal-load';
 import TableLink from './_table-link';
 import ToggleSelect from "./_toggle-select"; 
 import ToggleSwitch from "./_toggle-switch"; 
 
-// Init the ToggleSwitch
-var switchs = document.querySelectorAll('.toggle-switch');
-if(switchs)
+// Init the Autocomplete
+var autocompletes = document.querySelectorAll('.autocomplete')
+if(autocompletes)
 {
-    switchs.forEach(el => new ToggleSwitch(el));
+    autocompletes.forEach(el => new Autocomplete(el));
 }
 
-// Init the ToggleSelect
-var selects = document.querySelectorAll('.toggle-select');
-if(selects)
+// Init the Calendar
+var calendars = document.querySelectorAll('.calendar-automatic')
+if(calendars)
 {
-    selects.forEach(el => new ToggleSelect(el));
-}
-
-// Init the Table Link
-var tableLinks = document.querySelectorAll('.table-link');
-if(tableLinks)
-{
-    tableLinks.forEach(el => new TableLink(el));
-}
-
-// Init the Dynamic
-var dynamics = document.querySelectorAll('.dynamic-automatic');
-if(dynamics)
-{
-    dynamics.forEach(el => new Dynamic(el));
-}
-
-// Init the Modal Load
-var modalsToLoad = document.querySelectorAll('.modal-load');
-if(modalsToLoad)
-{
-    modalsToLoad.forEach(el => new ModalLoad(el));
-}
-
-// Init the Modal Confirm
-var modalsToConfirm = document.querySelectorAll('.modal-confirm');
-if(modalsToConfirm)
-{
-    modalsToConfirm.forEach(el => new ModalConfirm(el));
+  calendars.forEach(el => new Calendar(el));
 }
 
 // Init the Checkbox All
@@ -66,26 +37,18 @@ if(checkboxes)
     checkboxes.forEach(el => new CheckboxAll(el));
 }
 
-
-// Init the Calendar
-var calendars = document.querySelectorAll('.calendar-automatic')
-if(calendars)
-{
-  calendars.forEach(el => new Calendar(el));
-}
-
-// Init the Autocomplete
-var autocompletes = document.querySelectorAll('.autocomplete')
-if(autocompletes)
-{
-    autocompletes.forEach(el => new Autocomplete(el));
-}
-
 // Init the Datepicker
 var datepickers = document.querySelectorAll('.datepicker-automatic');
 if(datepickers)
 {
     datepickers.forEach(el => new Datepicker(el));
+}
+
+// Init the Dynamic
+var dynamics = document.querySelectorAll('.dynamic-automatic');
+if(dynamics)
+{
+    dynamics.forEach(el => new Dynamic(el));
 }
 
 // Init the Editor
@@ -109,6 +72,37 @@ if(editors)
     });
 }
 
+// Init the Modal Confirm
+var modalsToConfirm = document.querySelectorAll('.modal-confirm');
+if(modalsToConfirm)
+{
+    modalsToConfirm.forEach(el => new ModalConfirm(el));
+}
 
+// Init the Modal Load
+var modalsToLoad = document.querySelectorAll('.modal-load');
+if(modalsToLoad)
+{
+    modalsToLoad.forEach(el => new ModalLoad(el));
+}
 
+// Init the Table Link
+var tableLinks = document.querySelectorAll('.table-link');
+if(tableLinks)
+{
+    tableLinks.forEach(el => new TableLink(el));
+}
 
+// Init the ToggleSelect
+var selects = document.querySelectorAll('.toggle-select');
+if(selects)
+{
+    selects.forEach(el => new ToggleSelect(el));
+}
+
+// Init the ToggleSwitch
+var switchs = document.querySelectorAll('.toggle-switch');
+if(switchs)
+{
+    switchs.forEach(el => new ToggleSwitch(el));
+}
