78a016e041e52c5262800b0362ae66749db15d50
Natacha Herth <info@natachaherth.ch>
2023-01-19 12:57:23 +0100

    Pint

--- config/bs-component.php
+++ config/bs-component.php
@@ -12,18 +12,18 @@ return [
     */
 
     'modal-confirm' => [
-      'buttons' => [
-        'cancel'  => [
-          'class' => 'btn-outline-secondary',
-          'label' => 'bs-component::button.cancel',
-          'value' => 'bs-component::button.cancel',
+        'buttons' => [
+            'cancel' => [
+                'class' => 'btn-outline-secondary',
+                'label' => 'bs-component::button.cancel',
+                'value' => 'bs-component::button.cancel',
+            ],
+            'confirm' => [
+                'class' => 'btn-', // The color of the button is defined by the component option
+                'label' => 'bs-component::button.confirm',
+                'value' => 'bs-component::button.confirm',
+            ],
         ],
-        'confirm'  => [
-          'class' => 'btn-', // The color of the button is defined by the component option
-          'label' => 'bs-component::button.confirm',
-          'value' => 'bs-component::button.confirm',
-        ]
-      ]
     ],
 
     /*
@@ -36,28 +36,28 @@ return [
     */
 
     'dynamic' => [
-      'buttons' => [
-        'add'  => [
-          'class' => 'btn-primary',
-          'label' => 'bs-component::button.add',
-          'value' => 'bs-component::button.add',
+        'buttons' => [
+            'add' => [
+                'class' => 'btn-primary',
+                'label' => 'bs-component::button.add',
+                'value' => 'bs-component::button.add',
+            ],
+            'remove' => [
+                'class' => 'btn-danger',
+                'label' => 'bs-component::button.remove',
+                'value' => 'bs-component::button.remove',
+            ],
+            'delete' => [
+                'class' => 'btn-outline-danger',
+                'label' => 'bs-component::button.delete',
+                'value' => 'bs-component::button.delete',
+            ],
+            'move' => [
+                'class' => 'btn-info',
+                'label' => 'bs-component::button.move',
+                'value' => 'bs-component::button.move',
+            ],
         ],
-        'remove'  => [
-          'class' => 'btn-danger',
-          'label' => 'bs-component::button.remove',
-          'value' => 'bs-component::button.remove',
-        ],
-        'delete'  => [
-          'class' => 'btn-outline-danger',
-          'label' => 'bs-component::button.delete',
-          'value' => 'bs-component::button.delete',
-        ],
-        'move'  => [
-          'class' => 'btn-info',
-          'label' => 'bs-component::button.move',
-          'value' => 'bs-component::button.move',
-        ]
-      ]
     ],
 
     /*
@@ -70,15 +70,13 @@ return [
     */
 
     'editor' => [
-      'toolbar'    => ['font','div','format','list','link','color'],
-      'headings'   => ['1','2','3'],
-      'paragraphs' => ['lead','p'],
-      'divs'       => ['blockquote'],
-      'formats'    => ['bold','italic','underline','strike'],
-      'colors'     => ['primary','success','warning','danger'],
-      'emojis'     => ['bi-emoji-smile','bi-emoji-neutral','bi-emoji-frown','bi-emoji-heart-eyes','bi-emoji-wink','bi-hand-thumbs-up','bi-hand-thumbs-down']
+        'toolbar' => ['font', 'div', 'format', 'list', 'link', 'color'],
+        'headings' => ['1', '2', '3'],
+        'paragraphs' => ['lead', 'p'],
+        'divs' => ['blockquote'],
+        'formats' => ['bold', 'italic', 'underline', 'strike'],
+        'colors' => ['primary', 'success', 'warning', 'danger'],
+        'emojis' => ['bi-emoji-smile', 'bi-emoji-neutral', 'bi-emoji-frown', 'bi-emoji-heart-eyes', 'bi-emoji-wink', 'bi-hand-thumbs-up', 'bi-hand-thumbs-down'],
     ],
-
-
 
 ];
